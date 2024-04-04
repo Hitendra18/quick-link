7fdd63701c129df15c13b867ace698ea35759fd1
Hitendra <hitendraverma001@gmail.com>
2024-04-04 15:05:32 +0530

    completed

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -149,7 +149,7 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
                                 <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">QuickLink inc.</span>
-                                <br> Want to share a file? <a href="https://quick-link-alpha.vercel.app/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">QuickLink</a>.
+                                <br> Want to share a file? <a href="http://localhost:3000/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">QuickLink</a>.
                             </td>
                             </tr>
                         </table>
